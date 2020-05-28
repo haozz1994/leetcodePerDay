df8bbefe58b707d01bb4857035e98f523a38788d
haozz <haozz0611@163.com>
2020-05-28 22:28:38 +0800

    init

---
+++ src/main/java/com/haozz/leetcode/DemoController.java
@@ -0,0 +1,14 @@
+package com.haozz.leetcode;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
