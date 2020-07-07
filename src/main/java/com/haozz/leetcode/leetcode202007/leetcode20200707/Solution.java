001f1f1b1e0ee9dce5f516f339a43d77bb82f43b
haozz <haozz0611@163.com>
2020-07-07 22:29:31 +0800

    solution

---
+++ src/main/java/com/haozz/leetcode/leetcode202007/leetcode20200707/Solution.java
@@ -0,0 +1,98 @@
+package com.haozz.leetcode.leetcode202007.leetcode20200707;
+
+/**
+ *剑指 Offer 04. 二维数组中的查找
+ * https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+ *
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 现有矩阵 matrix 如下：
+ *
+ * [
+ *   [1,   4,  7, 11, 15],
+ *   [2,   5,  8, 12, 19],
+ *   [3,   6,  9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ *
+ * 给定 target = 20，返回 false。
+ *
+ *  
+ *
+ * 限制：
+ *
+ * 0 <= n <= 1000
+ *
+ * 0 <= m <= 1000
+ *
+ */
+public class Solution {
+
+    public boolean findNumberIn2DArray(int [][] matrix, int target){
+
+        /**
+         * 暴力穷举
+         */
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                if(anInt == target){
+                    return true;
+                }
+            }
+        }
+        /**
+         * 没想到的是，暴力穷举法耗时居然不高
+         * 执行用时：
+         * 0 ms
+         * , 在所有 Java 提交中击败了
+         * 100.00%
+         * 的用户
+         * 内存消耗：
+         * 45.6 MB
+         * , 在所有 Java 提交中击败了
+         * 100.00%
+         * 的用户
+         */
+
+
+        return false;
+    }
+
+
+
+    //=====以下是官方解答======//
+
+    /**
+     * 方法一：暴力
+     * 如果不考虑二维数组排好序的特点，则直接遍历整个二维数组的每一个元素，判断目标值是否在二维数组中存在。
+     *
+     * 依次遍历二维数组的每一行和每一列。如果找到一个元素等于目标值，则返回 true。如果遍历完毕仍未找到等于目标值的元素，则返回 false。
+     *
+     * 复杂度分析
+     *
+     * 时间复杂度：O(nm)O(nm)。二维数组中的每个元素都被遍历，因此时间复杂度为二维数组的大小。
+     * 空间复杂度：O(1)O(1)。
+     *
+     */
+    public boolean findNumberIn2DArray_1(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
