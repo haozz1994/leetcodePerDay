78fd9e3ce671919644b1a91acdcbb9b8ebaa744c
haozz <haozz0611@163.com>
2020-07-22 22:01:43 +0800

    solution

--- src/main/java/com/haozz/leetcode/leetcode202007/leetcode20200720/Solution.java
+++ src/main/java/com/haozz/leetcode/leetcode202007/leetcode20200720/Solution.java
@@ -82,4 +82,58 @@ public class Solution {
      * 如果直到落子完毕仍然没有玩家获胜，那么根据数组 move 的长度返回平局 Draw 或游戏未结束 Pending。
      *
      */
+
+
+    // 以下是C语言解法
+//    class Solution {
+//        public:
+//        bool checkwin(unordered_set<int>& S, vector<vector<int>>& wins) {
+//            for (auto win: wins) {
+//                bool flag = true;
+//                for (auto pos: win) {
+//                    if (!S.count(pos)) {
+//                        flag = false;
+//                        break;
+//                    }
+//                }
+//                if (flag) {
+//                    return true;
+//                }
+//            }
+//            return false;
+//        }
+//
+//        string tictactoe(vector<vector<int>>& moves) {
+//            vector<vector<int>> wins = {
+//                    {0, 1, 2},
+//                    {3, 4, 5},
+//                    {6, 7, 8},
+//                    {0, 3, 6},
+//                    {1, 4, 7},
+//                    {2, 5, 8},
+//                    {0, 4, 8},
+//                    {2, 4, 6}
+//            };
+//
+//            unordered_set<int> A, B;
+//            for (int i = 0; i < moves.size(); ++i) {
+//                int pos = moves[i][0] * 3 + moves[i][1];
+//                if (i % 2 == 0) {
+//                    A.insert(pos);
+//                    if (checkwin(A, wins)) {
+//                        return "A";
+//                    }
+//                }
+//                else {
+//                    B.insert(pos);
+//                    if (checkwin(B, wins)) {
+//                        return "B";
+//                    }
+//                }
+//            }
+//
+//            return (moves.size() == 9 ? "Draw" : "Pending");
+//        }
+//    };
+
 }
