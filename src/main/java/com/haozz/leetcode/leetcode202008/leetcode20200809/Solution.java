e0c023f1cca634c95be573ff0bf2de07a67e5d39
haozz <haozz0611@163.com>
2020-08-09 22:27:52 +0800

    solution

---
+++ src/main/java/com/haozz/leetcode/leetcode202008/leetcode20200809/Solution.java
@@ -0,0 +1,39 @@
+package com.haozz.leetcode.leetcode202008.leetcode20200809;
+
+/**
+ * https://leetcode-cn.com/problems/add-binary/
+ * 67. 二进制求和
+ *
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ *
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ *
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ *  
+ *
+ * 提示：
+ *
+ * 每个字符串仅由字符 '0' 或 '1' 组成。
+ * 1 <= a.length, b.length <= 10^4
+ * 字符串如果不是 "0" ，就都不含前导零。
+ *
+ *
+ *
+ * @author haozhezhe@yunquna.com
+ * @date 22:23 2020-08-09
+ */
+public class Solution {
+
+    public String addBinary(String a, String b) {
+
+    }
+}
