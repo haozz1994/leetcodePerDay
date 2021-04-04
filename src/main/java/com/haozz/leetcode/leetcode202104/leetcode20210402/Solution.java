37d5c988b7fa3fed9d19a11a6c25f29f6e20991c
haozz <haozz0611@163.com>
2021-04-04 22:14:35 +0800

    solution

---
+++ src/main/java/com/haozz/leetcode/leetcode202104/leetcode20210402/Solution.java
@@ -0,0 +1,43 @@
+package com.haozz.leetcode.leetcode202104.leetcode20210402;
+
+/**
+ * 栈
+ * 496
+ * https://leetcode-cn.com/problems/next-greater-element-i/
+ *
+ * @author haozhezhe@yunquna.com
+ * @date 5:26 PM 4/2/21
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int [] nums1 = {4,1,2};
+        int [] nums2 = {1,3,4,2};
+        for (int i : nextGreaterElement(nums1, nums2)) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int result[] = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            outer:
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == nums1[i]) {
+                    if (j == nums2.length - 1) {
+                        result[i] = -1;
+                    } else {
+                        for (int k = j + 1; k < nums2.length; k++) {
+                            if (nums2[k] > nums1[i]) {
+                                result[i] = nums2[k];
+                                break outer;
+                            }
+                        }
+                        result[i] = -1;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
