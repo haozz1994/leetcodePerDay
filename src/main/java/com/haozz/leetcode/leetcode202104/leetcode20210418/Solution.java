aaa94b82b0283125359f200dbf6bf88f44e363fa
haozz <haozz0611@163.com>
2021-04-30 10:46:01 +0800

    solution

--- src/main/java/com/haozz/leetcode/leetcode202104/leetcode20210418/Solution.java
+++ src/main/java/com/haozz/leetcode/leetcode202104/leetcode20210418/Solution.java
@@ -2,6 +2,7 @@ package com.haozz.leetcode.leetcode202104.leetcode20210418;
 
 /**
  * https://leetcode-cn.com/problems/binary-search/
+ * https://leetcode-cn.com/problems/binary-search/
  * <p>
  * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
  * <p>
